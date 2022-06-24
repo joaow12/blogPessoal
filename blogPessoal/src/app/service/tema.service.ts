495601b16c4bff99ea56d597a799d249db068959
João Victor <jujotasurf@gmail.com>
2022-06-24 19:29:52 -0300

    implementação do put e delete de temas

--- blogPessoal/src/app/service/tema.service.ts
+++ blogPessoal/src/app/service/tema.service.ts
@@ -18,12 +18,24 @@ export class TemaService {
   //Metodo para mostrar todos os temas, puxando direto do back-end, nesse caso utiliza uma lista, pois é mais de um objeto
   getAllTema():Observable<Tema[]> {
     //Retornando a url e o token para confirmação
-    return this.http.get<Tema[]>('http://localhost:8080/temas', this.token)
+    return this.http.get<Tema[]>('http://localhost:8080/tema', this.token)
+  }
+
+  getByIdTema(id: number):Observable<Tema>{
+    return this.http.get<Tema>(`http//localhost:8080/tema/${id}`, this.token)
   }
 
   //Metodo para dar POST em um Tema, puxando direto do back-end
   postTema(tema: Tema):Observable<Tema> {
     //Retornando o URL do método, o objeto Tema, e o tekon para confirmar
-    return this.http.post<Tema>('http://localhost:8080/temas', tema, this.token)
+    return this.http.post<Tema>('http://localhost:8080/tema', tema, this.token)
+  }
+
+  putTema(tema: Tema):Observable<Tema> {
+    return this.http.put<Tema>('http://localhost:8080/tema', tema, this.token)
+  }
+
+  deleteTema(id: number){
+    return this.http.delete(`http://localhost:8080/tema/${id}`, this.token)
   }
 }
